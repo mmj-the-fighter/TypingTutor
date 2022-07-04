36644c5bcf869138537dccd5f495845d87f5091e
Manoj M J <mmjthefighter@gmail.com>
2022-07-04 11:29:01 +0530

    first commit

---
+++ TypingTutor.cpp
@@ -0,0 +1,202 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
+#include<limits>
+#define MAXCHARSPERLINE 80
+
+
+enum {
+	AlphabetsBit = 1,
+	SpecialCharactersBit = 2,
+	DigitsBit = 4
+};
+
+class RandomTextGenerator
+{
+	const char* atozSentence = "the quick brown fox jumps over a lazy dog";
+	const char* alphabets = "abcdefghijklmnopqrstuvwxyz";
+	const char* specialChars = " `~!@#$%^&*()-_=+[{]}\\|;:\"',<.>/?";
+	const char* digits = "0123456789";
+
+	int numOfAlphabets;
+	int numOfSpecialChars;
+	int numOfDigits;
+	std::random_device seeder;
+	std::mt19937 generator;
+	std::uniform_int_distribution<int> distribution;
+public:
+	RandomTextGenerator() {
+		numOfAlphabets = strlen(alphabets);
+		numOfSpecialChars = strlen(specialChars);
+		numOfDigits = strlen(digits);
+		generator.seed(seeder());
+	}
+	inline int RandomInRange(int min, int max) {
+		distribution.param(std::uniform_int_distribution<int>::param_type(min, max-1));
+		return distribution(generator);
+	}
+	void GenText(char* buffer, int bufferLength, int selectionBits) {
+		bool AreAlphabetsRequired = (selectionBits & AlphabetsBit) ? true : false;
+		bool AreSpecialCharsRequired = (selectionBits & SpecialCharactersBit) ? true : false;
+		bool AreDigitsRequired = (selectionBits & DigitsBit) ? true : false;
+		bufferLength = 5 + RandomInRange(0, bufferLength - 5);
+		int choice;
+		int lastSpaceIndex = -1;
+		int i = 0;
+		while (i < bufferLength - 1)
+		{
+			if (i - lastSpaceIndex > 3) {
+				if (RandomInRange(0, 4) == 0){
+					buffer[i] = ' ';
+					lastSpaceIndex = i;
+					++i;
+					continue;
+				}
+			}
+
+			if (AreAlphabetsRequired && AreSpecialCharsRequired && AreDigitsRequired)
+			{
+				choice = RandomInRange(0, 3);
+				if (choice == 0)
+					buffer[i] = alphabets[RandomInRange(0, numOfAlphabets)];
+				else if (choice == 1)
+					buffer[i] = specialChars[RandomInRange(0, numOfSpecialChars)];
+				else
+					buffer[i] = digits[RandomInRange(0, numOfDigits)];
+			}
+			else if (!AreAlphabetsRequired && AreSpecialCharsRequired && AreDigitsRequired) {
+				if (RandomInRange(0, 2) == 0)
+					buffer[i] = specialChars[RandomInRange(0, numOfSpecialChars)];
+				else
+					buffer[i] = digits[RandomInRange(0, numOfDigits)];
+			}
+			else if (AreAlphabetsRequired && !AreSpecialCharsRequired && AreDigitsRequired) {
+				if (RandomInRange(0, 2) == 0)
+					buffer[i] = alphabets[RandomInRange(0,numOfAlphabets)];
+				else
+					buffer[i] = digits[RandomInRange(0, numOfDigits)];
+			}
+			else if (AreAlphabetsRequired && AreSpecialCharsRequired && !AreDigitsRequired) {
+				if (RandomInRange(0, 2) == 0)
+					buffer[i] = alphabets[RandomInRange(0, numOfAlphabets)];
+				else
+					buffer[i] = specialChars[RandomInRange(0, numOfSpecialChars)];
+			}
+			else if (!AreAlphabetsRequired && !AreSpecialCharsRequired && AreDigitsRequired) {
+				buffer[i] = digits[RandomInRange(0, numOfDigits)];
+			}
+			else if (AreAlphabetsRequired && !AreSpecialCharsRequired && !AreDigitsRequired) {
+				buffer[i] = alphabets[RandomInRange(0, numOfAlphabets)];
+			}
+			else if (!AreAlphabetsRequired && AreSpecialCharsRequired && !AreDigitsRequired) {
+				buffer[i] = specialChars[RandomInRange(0, numOfSpecialChars)];
+			}
+			else{
+				if (RandomInRange(0, 2) == 0)
+					buffer[i] = alphabets[RandomInRange(0, numOfAlphabets)];
+				else
+					buffer[i] = digits[RandomInRange(0, numOfDigits)];
+			}
+			if (i > 0 && buffer[i - 1] == ' ' && isalpha(buffer[i]) && RandomInRange(0, 6) == 0)
+				buffer[i] = toupper(buffer[i]);
+			++i;
+		}
+		buffer[bufferLength - 1] = '\0';
+	}
+};
+
+class TypingTutor
+{
+
+public:
+	void MainLoop();
+private:
+	RandomTextGenerator rtg;	char c[2];
+
+};
+
+void TypingTutor::MainLoop()
+{
+	char ch, lineBuffer[MAXCHARSPERLINE];
+	int linebufferLength, index;
+	time_t startTime, endTime;
+	double timeElapsed, practiceDuration, charsPerSecond, wordsPerMinute, accuracy;
+	int numOfCharsTyped, errorsOccured;
+	int selectionBits;
+
+	std::cout << "Welcome to random text typing tutor" << std::endl;
+	while (true)
+	{
+		std::cout << "Enter intended practice duration in seconds: ";
+		(std::cin >> practiceDuration).get();
+
+		selectionBits = 0;
+		std::cout << "Do you want to practice alphabets(y/n): ";
+		(std::cin.get(ch)).get();
+		if (ch == 'y' || ch == 'Y')
+			selectionBits |= AlphabetsBit;
+
+		std::cout << "Do you want to practice special characters(y/n): ";
+		(std::cin.get(ch)).get();
+		if (ch == 'y' || ch == 'Y')
+			selectionBits |= SpecialCharactersBit;
+
+		std::cout << "Do you want to practice digits(y/n): ";
+		(std::cin.get(ch)).get();
+		if (ch == 'y' || ch == 'Y')
+			selectionBits |= DigitsBit;
+
+		numOfCharsTyped = 0;
+		errorsOccured = 0;
+		startTime = time(NULL);
+		do
+		{
+			rtg.GenText(lineBuffer, MAXCHARSPERLINE, selectionBits);
+			std::cout << lineBuffer << std::endl;
+			index = 0;
+			linebufferLength = strlen(&lineBuffer[0]);
+			do
+			{
+				ch = std::cin.get();
+				if (ch == '\n') {
+					break;
+				}
+				++numOfCharsTyped;
+				if (ch != lineBuffer[index++]) {
+					++errorsOccured;
+				}
+			} while (index < linebufferLength);
+			endTime = time(NULL);
+			timeElapsed = difftime(endTime, startTime);
+		} while (timeElapsed <= practiceDuration);
+		if (numOfCharsTyped == 0)
+			accuracy = 0;
+		else
+			accuracy = ((numOfCharsTyped - errorsOccured) / (float)numOfCharsTyped) * 100.0;
+		charsPerSecond = numOfCharsTyped / timeElapsed;
+		wordsPerMinute = charsPerSecond * 60.0 / 5.0;
+
+		std::cout << "Accuracy(%) " << accuracy << std::endl
+			<< "Speed(CPS): " << charsPerSecond << std::endl
+			<< "Speed(WPM): " << wordsPerMinute << std::endl;
+
+		std::cout << "\nTest finished\n";
+		std::cout << "Do you want to take another test? y/n :";
+		c[0] = std::cin.get();
+		c[1] = std::cin.get();
+		if (c[0] == 'n' || c[0] == 'N' || c[1] == 'n' || c[1] == 'N')
+			break;
+		else
+			continue;
+	}
+
+}
+
+int main()
+{
+	TypingTutor tt;
+	tt.MainLoop();
+	return 0;
+}
